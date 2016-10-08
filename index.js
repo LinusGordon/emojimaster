5f91b4d8ad8bb14dc21c4625df26de09605b1e9f
Linus Gordon <linusgordon@gmail.com>
2016-10-08 11:24:03 -0400

    debugging

--- index.js
+++ index.js
@@ -40,10 +40,15 @@ app.get('/', function (req, res) {
 		let sender = event.sender.id;
 		if (event.message && event.message.text) {
 		    let text = event.message.text.toLowerCase();
+		    sendTextMessage(sender, text);
 		    text.replace(/\W/g, '') // remove all non-alphanumeric characters
+		    sendTextMessage(sender, text);
 		    text.replace(/emoji/g,''); // remove "emoji" from string
+		    sendTextMessage(sender, text);
 		    text.replace(/face/g,''); // remove "face" from string
+		    sendTextMessage(sender, text);
 		    text = text.split(' ').join('_'); // replace spaces with underscores
+		    sendTextMessage(sender, text);
 		    if(text == "what is your name?" || text == "what is your name") {
 				sendTextMessage(sender, "EmojiMaster, built by Linus");
 		    }		    
